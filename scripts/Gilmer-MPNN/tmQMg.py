100fcb1b81d32e4bd082862d3fdf4fad02f7949e
Hannes Kneiding <33654921+hkneiding@users.noreply.github.com>
2024-10-16 14:49:00 +0200

    updated graph file source locations

--- scripts/Gilmer-MPNN/tmQMg.py
+++ scripts/Gilmer-MPNN/tmQMg.py
@@ -12,9 +12,9 @@ from HyDGL import Graph
 
 
 RAW_URLS = [
-    'https://ns9999k.webs.sigma2.no/10.11582_2022.00057/nird/home/hanneskn/tmQMg/baseline_graphs.zip',
-    'https://ns9999k.webs.sigma2.no/10.11582_2022.00057/nird/home/hanneskn/tmQMg/uNatQ_graphs.zip',
-    'https://ns9999k.webs.sigma2.no/10.11582_2022.00057/nird/home/hanneskn/tmQMg/dNatQ_graphs.zip'
+    'https://ns9999k.webs.sigma2.no/10.11582_2024.00139/nird/home/hanneskn/tmQMg/baseline_graphs.zip',
+    'https://ns9999k.webs.sigma2.no/10.11582_2024.00139/nird/home/hanneskn/tmQMg/uNatQ_graphs.zip',
+    'https://ns9999k.webs.sigma2.no/10.11582_2024.00139/nird/home/hanneskn/tmQMg/dNatQ_graphs.zip'
 ]
 
 class tmQMg(Dataset):
